0ec99caf68c4dda7c785467dae2aac919541b77f
Justicesibisi <djmangsa@gmail.com>
2024-05-08 08:47:50 -0700

    Algorithms

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,45 +1,45 @@
 #include "search_algos.h"
-void atos(int *array, size_t from, size_t to);
-/**
- * advanced_binary - Binary search for a value in a sorted array
- * @array: it's a pointer to the first element of the array to search in
- * @size: it's the number of elements in array
- * @value: it's the value to search for
- * Return: first index where value is located.
- */
-int advanced_binary(int *array, size_t size, int value)
-{
-	size_t begining = 0, ending = size - 1;
 
-	if (array)
-		while (begining <= ending)
+/**
+ * jump_list - Searches for an algorithm in a sorted singly
+ *             linked list of integers using jump search.
+ * @list: A pointer to the  head of the linked list to search.
+ * @size: The number of nodes in the list.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         Otherwise, a pointer to the first node where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t step, step_size;
+	listint_t *node, *jump;
+
+	if (list == NULL || size == 0)
+		return (NULL);
+
+	step = 0;
+	step_size = sqrt(size);
+	for (node = jump = list; jump->index + 1 < size && jump->n < value;)
+	{
+		node = jump;
+		for (step += step_size; jump->index < step; jump = jump->next)
 		{
-			size_t middle = (ending - begining) / 2 + begining;
-
-			atos(array, begining, ending);
-			if (array[middle] == value)
-				return (middle);
-			if (value > array[middle])
-				begining = middle + 1;
-			else
-				ending = middle - 1;
+			if (jump->index + 1 == size)
+				break;
 		}
-	return (-1);
-}
+		printf("Value checked at index [%ld] = [%d]\n", jump->index, jump->n);
+	}
 
-/**
- * atos - Print an array or sub-array
- * @array: it's a pointer to the first element of the array to search in
- * @from: it's the number of elements in array
- * @to: it's the value to search for
- * Return: first index where value is located.
- */
-void atos(int *array, size_t from, size_t to)
-{
-	size_t i;
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
 
-	printf("Searching in array: ");
-	for (i = from; i < to; i++)
-		printf("%d, ", array[i]);
-	printf("%d\n", array[i]);
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
 }
--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,45 +1,46 @@
 #include "search_algos.h"
-void atos(int *array, size_t from, size_t to);
-/**
- * advanced_binary - Binary search for a value in a sorted array
- * @array: it's a pointer to the first element of the array to search in
- * @size: it's the number of elements in array
- * @value: it's the value to search for
- * Return: first index where value is located.
- */
-int advanced_binary(int *array, size_t size, int value)
-{
-	size_t begining = 0, ending = size - 1;
 
-	if (array)
-		while (begining <= ending)
+/**
+ * linear_skip - Searches for an algorithm in a sorted singly
+ *               linked list of integers using linear skip.
+ * @list: A pointer to the  head of the linked list to search.
+ * @value: The value to search for.
+ *
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         Otherwise, a pointer to the first node where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node, *jump;
+
+	if (list == NULL)
+		return (NULL);
+
+	for (node = jump = list; jump->next != NULL && jump->n < value;)
+	{
+		node = jump;
+		if (jump->express != NULL)
 		{
-			size_t middle = (ending - begining) / 2 + begining;
-
-			atos(array, begining, ending);
-			if (array[middle] == value)
-				return (middle);
-			if (value > array[middle])
-				begining = middle + 1;
-			else
-				ending = middle - 1;
+			jump = jump->express;
+			printf("Value checked at index [%ld] = [%d]\n",
+					jump->index, jump->n);
 		}
-	return (-1);
-}
+		else
+		{
+			while (jump->next != NULL)
+				jump = jump->next;
+		}
+	}
 
-/**
- * atos - Print an array or sub-array
- * @array: it's a pointer to the first element of the array to search in
- * @from: it's the number of elements in array
- * @to: it's the value to search for
- * Return: first index where value is located.
- */
-void atos(int *array, size_t from, size_t to)
-{
-	size_t i;
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
 
-	printf("Searching in array: ");
-	for (i = from; i < to; i++)
-		printf("%d, ", array[i]);
-	printf("%d\n", array[i]);
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
 }
